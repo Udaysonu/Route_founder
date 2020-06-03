a1d8a62fb0865a568e374745af37a528115cdc61
Udaysonu <42859381+Udaysonu@users.noreply.github.com>
2020-06-03 13:19:31 +0530

    clean code-comment code@nodemailer.js

--- mailer/nodemailer.js
+++ mailer/nodemailer.js
@@ -1,6 +1,15 @@
 const nodemailer = require("nodemailer");
 const ejs=require("ejs")
 const path=require("path")
+
+
+//FUNCTIONS ARE WRITTEN IN FOLLOWING ORDER
+//1->signup_done 
+//2->booking_done
+//3->google_done
+
+
+
 // async..await is not allowed in global scope, must use a wrapper
 
   // Generate test SMTP service account from ethereal.email
@@ -15,35 +24,34 @@ const path=require("path")
     auth: {
       user: "EmailHere", // generated ethereal user
       pass: "Passwordhere" // generated ethereal password
-    }
+      }
   });
 
  
   // send mail with defined transport object
 var sender=function(data,relativepath){
-   
 
-   var renderTemplete=function(data,relativepath){
-    ejs.renderFile( path.join(__dirname,relativepath),data,function(err,template){
-      console.log(template)
-      if(err){
+    //rendering ejs file with relativepath and data to it;
+   var renderTemplete=function(data,relativepath)
+   {
+    ejs.renderFile( path.join(__dirname,relativepath),data,function(err,template)
+    {
+      if(err)
+      {
         console.log("Error in sending email:-",err);
-      }else{
+      }
+      else
+      {
         transporter.sendMail({
           from: 'udaysonubakka143@gmail.com', // sender address
           to: data.user.email , // list of receivers
           subject: "AeroBook", // Subject line
          // plain text body
           html: template// html body
-        });
-
-
-        
+                             });
       }
-          })
+    })
   }
-
-
   renderTemplete(data,relativepath);
 }
 //   console.log("Message sent: %s", info.messageId);
@@ -54,17 +62,17 @@ var sender=function(data,relativepath){
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
 
+//function to send mail when user signup is successfull
 module.exports.signup_done=function(data){
-
 sender(data,"../views/mail_templates/signup_successful.ejs")
-
-
 }
 
+//funciton to send mail when authentication using google is successfull
 module.exports.google_done=function(data){
-
   sender(data,"../views/mail_templates/google_auth.ejs")
 }
+
+//function to send mail when booking is confirmed
 module.exports.booking_done=function(data){
   sender(data,"../views/mail_templates/booking_confirmed.ejs");
 }
